1644b5f528a2ff6177568e11f3fdd66dcffa06ce
Hafiz Shoaib Siddiq <159219257+student-shoaibsoomro@users.noreply.github.com>
2024-03-25 10:40:47 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import PromptSync from "prompt-sync";
+const prompt = PromptSync();
+
+let userInput = prompt("Please Enter Your Name Here: ");
+console.log("Hello " + userInput + ', nice to meet you!');
+
+let num1 = parseInt(prompt('Please Enter your first number: '));
+let num2 = parseInt(prompt('Enter your second number: '));
+console.log(num1 + num2);
+console.log('Here is your answer ThankYou!');
